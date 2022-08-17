3cdce48663aad83db761178334a8834f9f7a44be
Charged <someonesomething800@gmail.com>
2022-08-17 15:44:29 -0400

    New GNU Emacs config.

---
+++ .emacs.d/init.el
@@ -0,0 +1,21 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(package-selected-packages
+   '(doom-modeline doom-themes use-package evil-tutor evil-collection))
+ '(warning-suppress-log-types '(((package reinitialization)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
