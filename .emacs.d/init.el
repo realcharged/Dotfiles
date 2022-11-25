8955e1518af7c70e24627cf572eff0d83b0dc0af
Charged <someonesomething800@gmail.com>
2022-11-25 15:03:38 -0500

    Add a few more things to config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -199,3 +199,22 @@
 
 ;; ;; Close stuff
 ;; (electric-pair-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(package-selected-packages
+   '(gcmh dashboard markdown-mode lua-mode python-mode vterm toc-org helm-rdefs org-bullets good-scroll beacon beacon-mode general evil-tutor evil-collection evil counsel doom-modeline doom-themes which-key all-the-icons rainbow-delimiters ivy use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 3.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 2.0))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 0.5))))
+ '(org-level-5 ((t (:inherit outline-5 :height 0.1)))))
