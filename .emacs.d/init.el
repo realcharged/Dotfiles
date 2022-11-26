60579b0c0e36f6444d31083909bf7047ff90178e
Charged <someonesomething800@gmail.com>
2022-11-25 19:20:31 -0500

    Add EAF!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,10 +22,14 @@
  '(custom-safe-themes
    '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(package-selected-packages
-   '(gcmh dashboard markdown-mode lua-mode python-mode vterm toc-org helm-rdefs org-bullets good-scroll beacon beacon-mode general evil-tutor evil-collection evil counsel doom-modeline doom-themes which-key all-the-icons rainbow-delimiters ivy use-package)))
+   '(visual-fill-column gcmh dashboard markdown-mode lua-mode python-mode vterm toc-org helm-rdefs org-bullets good-scroll beacon beacon-mode general evil-tutor evil-collection evil counsel doom-modeline doom-themes which-key all-the-icons rainbow-delimiters ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.25))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.2)))))
