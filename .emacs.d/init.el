06c98232728e7ed1405d085fef34f208b806661b
charged1 <someonesomething800@gmail.com>
2023-02-24 21:54:13 -0500

    Updates.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,7 +22,7 @@
  '(custom-safe-themes
    '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(package-selected-packages
-   '(company netease-cloud-music transient quelpa-use-package ement visual-fill-column gcmh dashboard markdown-mode lua-mode python-mode vterm toc-org helm-rdefs org-bullets good-scroll beacon beacon-mode general evil-tutor evil-collection evil counsel doom-modeline doom-themes which-key all-the-icons rainbow-delimiters ivy use-package)))
+   '(ligature company netease-cloud-music transient quelpa-use-package ement visual-fill-column gcmh dashboard markdown-mode lua-mode python-mode vterm toc-org helm-rdefs org-bullets good-scroll beacon beacon-mode general evil-tutor evil-collection evil counsel doom-modeline doom-themes which-key all-the-icons rainbow-delimiters ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
